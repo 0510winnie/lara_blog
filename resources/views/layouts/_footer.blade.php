b5c4513399f359c3cd3c871ec67bc00527035220
Luna <0510winnie@gmail.com>
2018-06-28 19:35:44 +0800

    finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            &copy; Your Website 2018. All Rights Reserved.
+        </p>
+
+    </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', '首頁')
+
+@section('content')
+<p>這裡是首頁</p>
+@endsection
